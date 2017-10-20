19c3bdd5909e7c3d1bf656bf6c537c3575fba886
Lê Minh Tuấn <tuanlm@jcs-corp.com>
2017-10-20 10:31:41 +0700

    fix

--- src/resources/views/layouts/admin.blade.php
+++ src/resources/views/layouts/admin.blade.php
@@ -484,7 +484,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0.9
     </div>
-    <strong>Copyright &copy; 2018 <a href="#">Dakmark Food</a>.</strong> All rights
+    <strong>Copyright &copy; 2018 <a href="#">Poko Farms</a>.</strong> All rights
     reserved.
   </footer>
 
