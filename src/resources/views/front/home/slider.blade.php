d6ea86247ad33962facd4f2b5f654fa80b8927a2
hieudt <hieudt0302@gmail.com>
2017-10-20 13:32:26 +0700

    Fixed post index 3

--- src/resources/views/front/home/slider.blade.php
+++ src/resources/views/front/home/slider.blade.php
@@ -10,7 +10,7 @@
 					<!-- LAYERS -->
 
 					<!-- LAYER NR. 1 -->
-					<div class="tgặpaption   tp-resizeme" 
+					<div class="tp-caption   tp-resizeme" 
 						 id="slide-10-layer-1" 
 						 data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" 
 						 data-y="['middle','middle','middle','middle']" data-voffset="['0','0','0','0']" 
@@ -30,9 +30,10 @@
 						data-paddingbottom="[0,0,0,0]"
 						data-paddingleft="[0,0,0,0]"
 
-						style="z-index: 5; white-space: nowrap; font-size: 64px; line-height: 64px; font-weight: 700; color: rgba(255, 255, 255, 1.00);font-family:Playfair Display;">{{$slider->translation->title}} </div>
+						style="z-index: 5; white-space: nowrap; font-size: 64px; line-height: 64px; font-weight: 700; color: rgba(255, 255, 255, 1.00);font-family:Playfair Display;">{{$slider->translation->description}} </div>
 
-					<!-- LAYER NR. 2 -->
+
+					<!-- LAYER NR. 6 -->
 					<div class="tp-caption btn-line rev-btn " 
 						 id="slide-10-layer-10" 
 						 data-x="['center','center','center','center']" data-hoffset="['-1','-1','-1','0']" 
@@ -51,7 +52,8 @@
 						data-paddingbottom="[14,14,14,14]"
 						data-paddingleft="[35,35,35,35]"
 
-						style="z-index: 10; white-space: nowrap; font-size: 17px; line-height: 17px; font-weight: 400; color: rgba(142, 179, 90, 1.00);font-family:Lato;text-transform:uppercase;background-color:rgba(255, 255, 255, 1.00);border-color:rgba(142, 179, 90, 0);border-style:solid;border-width:1px;border-radius:30px 30px 30px 30px;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;letter-spacing:0.1em;cursor:pointer;">@lang('common.more-details') </div>
+						style="z-index: 10; white-space: nowrap; font-size: 17px; line-height: 17px; font-weight: 400; color: rgba(142, 179, 90, 1.00);font-family:Lato;text-transform:uppercase;background-color:rgba(255, 255, 255, 1.00);border-color:rgba(142, 179, 90, 0);border-style:solid;border-width:1px;border-radius:30px 30px 30px 30px;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;letter-spacing:0.1em;cursor:pointer;">@lang('common.more-details') 
+					</div>
 				</li>
 				@endforeach
 			</ul>
