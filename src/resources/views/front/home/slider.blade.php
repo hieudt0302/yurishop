130afbd4a2338c23fcd412064ba971f45a78da38
hieudt <hieudt0302@gmail.com>
2017-10-23 00:08:23 +0700

    Added slide url

--- src/resources/views/front/home/slider.blade.php
+++ src/resources/views/front/home/slider.blade.php
@@ -53,7 +53,7 @@
 						data-paddingbottom="[14,14,14,14]"
 						data-paddingleft="[35,35,35,35]"
 
-						style="z-index: {{$slider->id}}6; white-space: nowrap; font-size: 17px; line-height: 17px; font-weight: 400; color: rgba(142, 179, 90, 1.00);font-family:Lato;text-transform:uppercase;background-color:rgba(255, 255, 255, 1.00);border-color:rgba(142, 179, 90, 0);border-style:solid;border-width:1px;border-radius:30px 30px 30px 30px;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;letter-spacing:0.1em;cursor:pointer;">@lang('common.more-details')</div>
+						style="z-index: {{$slider->id}}6; white-space: nowrap; font-size: 17px; line-height: 17px; font-weight: 400; color: rgba(142, 179, 90, 1.00);font-family:Lato;text-transform:uppercase;background-color:rgba(255, 255, 255, 1.00);border-color:rgba(142, 179, 90, 0);border-style:solid;border-width:1px;border-radius:30px 30px 30px 30px;outline:none;box-shadow:none;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;letter-spacing:0.1em;cursor:pointer;"><a href="/{{$slider->url}}" style="text-decoration:none">@lang('common.more-details')</a></div>
 				</li>
 				@endforeach
 			</ul>
