1e4b2d3b8f196805595a7a1aa1bc83549cbfca7c
bravohex <bravohex@outlook.com>
2017-10-29 22:10:26 +0700

    fuk

--- src/routes/web.php
+++ src/routes/web.php
@@ -231,7 +231,7 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'auth
     Route::post('orders/{id}/details/update', ['as'=>'admin.orders.show','uses'=>'OrdersController@DetailUpdateItem','middleware' => ['role:admin|manager']]);
     Route::post('orders/{id}/update/billingaddress',['as'=>'admin.orders.show','uses'=>'OrdersController@UpdateBillingAddress','middleware' => ['role:admin|manager']]);
     Route::post('orders/{id}/update/shippingaddress',['as'=>'admin.orders.show','uses'=>'OrdersController@UpdateShippingAddress','middleware' => ['role:admin|manager']]);
-    Route::delete('orders/{id}/details/{detail_id}',['as'=>'admin.products.show','uses'=>'ProductsController@DetailDestroy','middleware' => ['role:admin|manager']]);
+    Route::delete('orders/{id}/details/{detail_id}',['as'=>'admin.products.show','uses'=>'OrdersController@DetailDestroy','middleware' => ['role:admin|manager']]);
     // InfoPages
     Route::get('info-pages',['as'=>'admin.info-pages.index','uses'=>'InfoPagesController@index','middleware'=> ['role:admin|manager']]);
     Route::get('info-pages/create',['as'=>'admin.info-pages.create','uses'=>'InfoPagesController@create','middleware'=> ['role:admin|manager']]);
