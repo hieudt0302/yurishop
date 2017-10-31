c8b01683bc3d86f5ab648a9e8bfc6582c14c2a9b
bravohex <bravohex@outlook.com>
2017-10-31 22:53:49 +0700

    fix

--- src/resources/views/front/products/index.blade.php
+++ src/resources/views/front/products/index.blade.php
@@ -48,7 +48,7 @@
                         <div class="form-group">
                             <input type="text" name="search_content" placeholder="What are you looking for?"  class="form-control">
                         </div>
-                         <button type="submit">Search</button>
+                         <button type="submit" style="background-color: #fbfaf9;border: 1px solid #efefef;border-radius: 5px;">Search</button>
                     </form>
 				</div>
 			</div>
