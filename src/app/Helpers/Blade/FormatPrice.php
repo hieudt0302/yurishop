1d4e6f98e5e4fbe5737af0e06a4350fe17c22d9e
bravohex <bravohex@outlook.com>
2017-11-05 17:37:46 +0700

    fix

--- src/app/Helpers/Blade/FormatPrice.php
+++ src/app/Helpers/Blade/FormatPrice.php
@@ -3,13 +3,13 @@ namespace App\Helpers\Blade;
 
 class FormatPrice {
 
-    public static function price($price, $type) {
+    public static function price($price, $type = null) {
         switch ($type) {
             case 'vnd':
-                return number_format($price, 2, ',', '.'); //etc: 5,500,000.69 
+                return number_format($price, 2, '.', ','); //etc: 5,500,000.69 
                 break;
             default:
-                return number_format($price, 2, '.', ','); //etc: 5.500.000,69
+                return number_format($price, 2, ',', '.'); //etc: 5.500.000,69
         } 
     }
 }
