ec9e95b65d3ce105951cf04349ad7adbc529a4a8
Lê Minh Tuấn <tuanlm@jcs-corp.com>
2017-11-07 10:41:44 +0700

    fix menu/search

--- src/app/Http/Controllers/Front/MenuController.php
+++ src/app/Http/Controllers/Front/MenuController.php
@@ -17,20 +17,25 @@ class MenuController extends Controller
     {
         $search = Input::get('search');
         Input::flashOnly(['search']);
-
+        $category = Category::where('slug', $slug)->firstOrFail();
+        $category_id = $category->id;
+        
         if ($parent == "products") {
+
             //RELATED
-            $category = Category::where('slug', $slug)->firstOrFail();
             $tags = Tag::has('products')->get();
             $comments = Tag::has('products')->get();
             $lastProducts = Product::where('published',1)->take(4)->get(); 
-            
+
             // GET PRODUCTS
             $results = $category->publishedProducts()
             ->where('products.name', 'LIKE', '%'. $search . '%')
-            ->orWhereIn('products.id', function($query) use ($search){
+            ->orWhereIn('products.id', function($query) use ($search,$category_id){
                 $query->select('product_id')->from('product_translations')
-                ->Where('name','LIKE', '%'. $search . '%');
+				->leftJoin('products','product_translations.product_id','=','products.id')
+				->where('products.category_id', $category_id)
+                ->Where('product_translations.name','LIKE', '%'. $search . '%')
+				->groupBy('product_id');
             })
             ->paginate(21);  
 
@@ -38,8 +43,8 @@ class MenuController extends Controller
             ->with('i', ($page??1 - 1) * 21);
 
         } elseif ($parent == "posts") {
+
             //RELATED
-            $category = Category::where('slug', $slug)->firstOrFail();
             $tags = Tag::has('posts')->get();
             $comments = Tag::has('posts')->get();
             $lastPosts = Post::where('published',1)->take(4)->get(); 
@@ -52,6 +57,17 @@ class MenuController extends Controller
             ->orWhereIn('posts.id', function($query) use ($search){
                 $query->select('post_id')->from('post_translations')
                 ->Where('title','LIKE', '%'. $search . '%');
+            })
+            ->paginate(21); 
+
+            $posts = $category->publishedPosts()
+            ->where('posts.title', 'LIKE', '%'. $search . '%')
+            ->orWhereIn('posts.id', function($query) use ($search,$category_id){
+                $query->select('post_id')->from('post_translations')
+				->leftJoin('posts','post_translations.post_id','=','posts.id')
+				->where('posts.category_id', $category_id)
+                ->Where('post_translations.title','LIKE', '%'. $search . '%')
+				->groupBy('post_id');
             })
             ->paginate(21);  
             
