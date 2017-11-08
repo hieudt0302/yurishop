80aa0abb2c2a28a28342a365da3d1c248e719821
Lê Minh Tuấn <tuanlm@jcs-corp.com>
2017-11-08 10:26:37 +0700

    f

--- src/app/Http/Controllers/Admin/DashboardController.php
+++ src/app/Http/Controllers/Admin/DashboardController.php
@@ -13,8 +13,8 @@ class DashboardController extends Controller
     public function index()
     {
         $user_new = User::where('created_at', '>=', Carbon::today()->startOfWeek())->count();
-        $order_new = Order::where('status', 0)->count();
-        $order_wait = DB::table('orders')->where('status', 1)->count();
+        $order_new = Order::where('order_status', 0)->count();
+        $order_wait = DB::table('orders')->where('order_status', 1)->count();
 
         return View('admin.dashboard.index',compact('order_new','order_wait','user_new'));
     }
--- src/resources/views/admin/dashboard/index.blade.php
+++ src/resources/views/admin/dashboard/index.blade.php
@@ -53,14 +53,14 @@
       <!-- small box -->
       <div class="small-box bg-yellow">
         <div class="inner">
-          <h3>44</h3>
+          <h3>{{$user_new}}</h3>
 
-          <p>User Registrations</p>
+          <p>Người Dùng Mới</p>
         </div>
         <div class="icon">
           <i class="ion ion-person-add"></i>
         </div>
-        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        <a href="{{url('admin/users')}}" class="small-box-footer">>Chi Tiết<i class="fa fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
