04f80ee6e73afd99bb3514914bc2b63ec856dc7f
Lê Minh Tuấn <tuanlm@jcs-corp.com>
2017-11-08 10:23:04 +0700

    f

--- src/app/Http/Controllers/Admin/DashboardController.php
+++ src/app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\User;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return View('admin.dashboard.index');
+        $user_new = User::where('created_at', '>=', Carbon::today()->startOfWeek())->count();
+        $order_new = Order::where('status', 0)->count();
+        $order_wait = DB::table('orders')->where('status', 1)->count();
+
+        return View('admin.dashboard.index',compact('order_new','order_wait','user_new'));
     }
 }
--- src/resources/views/admin/dashboard/index.blade.php
+++ src/resources/views/admin/dashboard/index.blade.php
@@ -23,14 +23,14 @@
       <!-- small box -->
       <div class="small-box bg-aqua">
         <div class="inner">
-          <h3>150</h3>
+          <h3>{{$order_new}}</h3>
 
-          <p>New Orders</p>
+          <p>Đơn Hàng Mới</p>
         </div>
         <div class="icon">
           <i class="ion ion-bag"></i>
         </div>
-        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        <a href="{{url('admin/orders')}}" class="small-box-footer">Chi Tiết <i class="fa fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
@@ -38,14 +38,14 @@
       <!-- small box -->
       <div class="small-box bg-green">
         <div class="inner">
-          <h3>53<sup style="font-size: 20px">%</sup></h3>
+          <h3>{{$order_wait}}</h3>
 
-          <p>Bounce Rate</p>
+          <p>Đơn Hàng Chờ Giao</p>
         </div>
         <div class="icon">
           <i class="ion ion-stats-bars"></i>
         </div>
-        <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+        <a href="{{url('admin/orders')}}" class="small-box-footer">Chi Tiết <i class="fa fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
