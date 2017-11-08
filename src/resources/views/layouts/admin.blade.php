85064bc5a6f39287d0195b924de5e10fa97d58a0
Lê Minh Tuấn <tuanlm@jcs-corp.com>
2017-11-08 09:49:11 +0700

    f

--- src/resources/views/layouts/admin.blade.php
+++ src/resources/views/layouts/admin.blade.php
@@ -95,12 +95,12 @@
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
           <!-- Messages: style can be found in dropdown.less-->
-          <li class="dropdown messages-menu">
+          <!-- <li class="dropdown messages-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-envelope-o"></i>
               <span class="label label-success">4</span>
             </a>
-            <!-- <ul class="dropdown-menu">
+            <ul class="dropdown-menu">
               <li class="header">You have 4 messages</li>
               <li>
                 <ul class="menu">
@@ -167,15 +167,15 @@
                 </ul>
               </li>
               <li class="footer"><a href="#">See All Messages</a></li>
-            </ul> -->
-          </li>
+            </ul>
+          </li> -->
           <!-- Notifications: style can be found in dropdown.less -->
-          <li class="dropdown notifications-menu">
+          <!-- <li class="dropdown notifications-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-bell-o"></i>
               <span class="label label-warning">10</span>
             </a>
-            <!-- <ul class="dropdown-menu">
+            <ul class="dropdown-menu">
               <li class="header">You have 10 notifications</li>
               <li>
                 <ul class="menu">
@@ -208,15 +208,14 @@
                 </ul>
               </li>
               <li class="footer"><a href="#">View all</a></li>
-            </ul> -->
-          </li>
-          <!-- Tasks: style can be found in dropdown.less -->
-          <li class="dropdown tasks-menu">
+            </ul>
+          </li> -->
+          <!-- <li class="dropdown tasks-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-flag-o"></i>
               <span class="label label-danger">9</span>
             </a>
-            <!-- <ul class="dropdown-menu">
+            <ul class="dropdown-menu">
               <li class="header">You have 9 tasks</li>
               <li>
                 <ul class="menu">
@@ -281,8 +280,8 @@
               <li class="footer">
                 <a href="#">View all tasks</a>
               </li>
-            </ul> -->
-          </li>
+            </ul>
+          </li> -->
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -296,7 +295,7 @@
 
                 <p>
                 {{Auth::user()->last_name}} {{Auth::user()->first_name}} - Admin
-                  <small>Gia nhập từ {{ date('d/m/Y',strotime($Auth::user()->created_at))}}</small>
+                  <small>Gia nhập từ {{ date('d/m/Y',strtotime($Auth::user()->created_at))}}</small>
                 </p>
               </li>
               <!-- Menu Body -->
