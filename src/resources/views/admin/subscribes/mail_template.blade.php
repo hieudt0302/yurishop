c6f45c3ca91e1fad34cf8615896190f5dea73f45
ducthang237 <31162245+ducthang237@users.noreply.github.com>
2017-11-13 17:04:17 +0700

    Update mail_template.blade.php

--- src/resources/views/admin/subscribes/mail_template.blade.php
+++ src/resources/views/admin/subscribes/mail_template.blade.php
@@ -1,4 +1,4 @@
-<div>{{ $content }} </div>
+<div>{!! $content !!} </div>
 ----------------------------------
 <div>
 	<h3><a href="{{ url('/unsubscribe', $email)}}" target="_blank">@lang('footer.unsubscribe')</a></h3>
