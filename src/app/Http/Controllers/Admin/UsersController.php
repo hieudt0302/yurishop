25965b5d15d94ce46751eaeb2006e469458b4d03
bravohex <lmt.register@outlook.com>
2017-12-04 21:30:16 +0700

    update user

--- src/app/Http/Controllers/Admin/UsersController.php
+++ src/app/Http/Controllers/Admin/UsersController.php
@@ -116,7 +116,7 @@ class UsersController extends Controller
         $this->validate($request, [
             'username' => 'required',
             'email' => 'required|email|unique:users,email,'.$id,
-            'phone' => 'string',
+            'phone' => 'string|nullable',
             'password' => 'same:confirm_password',
             // 'roles' => 'required'
         ]);
