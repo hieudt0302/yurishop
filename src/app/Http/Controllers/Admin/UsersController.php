c41892a111eccc062f3cf103ddaa150e7fa2ea7c
bravohex <lmt.register@outlook.com>
2017-12-04 21:28:36 +0700

    update allow user:phone nullable

--- src/app/Http/Controllers/Admin/UsersController.php
+++ src/app/Http/Controllers/Admin/UsersController.php
@@ -46,7 +46,7 @@ class UsersController extends Controller
         $this->validate($request, [
             'username' => 'required',
             'email' => 'required|email|unique:users,email',
-            'phone' => 'string',
+            'phone' => 'string|nullable',
             'password' => 'required|same:confirm_password',
             // 'roles' => 'required'
         ]);
