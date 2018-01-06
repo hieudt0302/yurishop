f38824c490d37e869e4a28252af9da664d99d0c4
bravohex <lmt.register@outlook.com>
2018-01-06 13:11:52 +0700

    fix enter on cart form

--- src/resources/views/front/shoppingcart/cart.blade.php
+++ src/resources/views/front/shoppingcart/cart.blade.php
@@ -224,7 +224,10 @@
 
 <script type="text/javascript">
 $(document).ready(function(){   
-   
+    $('form input').on('keypress', function(e) {
+        return e.which !== 13;
+    });
+
     var orderSummary = $(".order-summary-content");
     orderSummary.on("click", ".btn-continue-shopping", function (e) {
         e.preventDefault();
