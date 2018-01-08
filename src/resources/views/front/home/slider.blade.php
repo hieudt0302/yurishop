81df95334a6017a9f81f1972075ed18482093fd2
Tôi Là Tùng <toilatung@icloud.com>
2018-01-08 17:41:26 +0700

    default

--- src/resources/views/front/home/slider.blade.php
+++ src/resources/views/front/home/slider.blade.php
@@ -46,7 +46,7 @@
 						data-type="button" 
 						data-responsive_offset="on" 
 						data-responsive="off"
-						data-frames='[{"from":"opacity:0;","speed":300,"to":"o:1;","delay":1400,"ease":"Power2.easeInOut"},{"delay":"wait","speed":300,"to":"opacity:0;","ease":"nothing"}]'
+						data-frames='[{"from":"opacity:0;","speed":300,"to":"o:1;","delay":1400,"ease":"Power2.easeInOut"},{"delay":"wait","speed":300,"to":"opacity:0;","ease":"nothing"},{"frame":"hover","speed":"0","ease":"Linear.easeNone","to":"o:1;rX:0;rY:0;rZ:0;z:0;","style":"c:rgba(255, 255, 255, 1.00);bg:rgba(142, 179, 90, 1.00);bw:1px 1px 1px 1px;"}]'
 						data-textAlign="['left','left','left','left']"
 						data-paddingtop="[14,14,14,14]"
 						data-paddingright="[35,35,35,35]"
