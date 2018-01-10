6e0947379261078e5b806d47adfde16ebf53c066
bravohex <lmt.register@outlook.com>
2018-01-10 22:56:33 +0700

    fix lang in show product

--- src/resources/views/front/products/show.blade.php
+++ src/resources/views/front/products/show.blade.php
@@ -309,7 +309,7 @@
                success:function(response){
                     console.log(response['newCartItemCount']); //debug
 					//$('.cartItemCount').html($('.cartItemCount').html().replace (/\((.*?)\)/g,"(" + response['newCartItemCount'] + ")"));
-					$('.cartItemCount').html(response['newCartItemCount'] + ' ' + @lang('shoppings.items'));
+					$('.cartItemCount').html(response['newCartItemCount'] + ' ' + {{trans('shoppings.items')}});
                },
                error:function(response){
                     console.log(response['newCartItemCount']); //debug
