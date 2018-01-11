e5761653839d45b5e4accc5f983260ad4252f9d0
Lê Minh Tuấn <tuanlm@jcs-corp.com>
2018-01-11 11:11:59 +0700

    fix phone not insert when register new user

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        dd($data['phone']);
+       
         return User::create([
             'username' => $data['username'],
             'first_name' => $data['first_name'],
