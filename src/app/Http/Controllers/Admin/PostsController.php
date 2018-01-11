e9a1079032e85a90900a0f2522bc04d4294794ad
Lê Minh Tuấn <tuanlm@jcs-corp.com>
2018-01-11 16:29:53 +0700

    dd

--- src/app/Http/Controllers/Admin/PostsController.php
+++ src/app/Http/Controllers/Admin/PostsController.php
@@ -356,7 +356,7 @@ class PostsController extends Controller
 
         //delete tag not use
         $tag_ids = DB::table('taggables')->get(['tag_id']);
-        //dd($tag_ids[0]);
+        dd($tag_ids->toArray());
         Tag::whereNotIn('id', $tag_ids->toArray())->delete();
 
     }
