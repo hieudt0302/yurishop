04c1ff570dfc5400b6c8c064cc5c382245cd2137
hieudt <hieudt0302@gmail.com>
2018-01-12 11:26:50 +0700

    Update about page

--- src/resources/views/front/home/about.blade.php
+++ src/resources/views/front/home/about.blade.php
@@ -4,38 +4,9 @@
 @section('content')
 
 <!-- Head Section -->
-<div class="hero">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1>{{$info_page_translation->title}}</h1>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- End Head Section -->
-<!-- <section id="decorate">
-    <div class="row">
-        <div class="container">
-            <div class="col-md-12 col-sm-12 col-xs-12">
-                <img src="frontend/images/logo2.png" alt="decorate">
-            </div>
-        </div>
-    </div>
-</section>
-<div class="about">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="ab-intro">
-                    <p>{!! $info_page_translation->description !!}</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
 
-<section id="aboutv1">
+
+<!-- <section id="aboutv1">
     <div class="container">
         <div class="row">
             <div class="decorate">
@@ -51,10 +22,78 @@
         </div>
     </div>
 </section>
+<div class="aboutflowerabv2 about floweraboutv4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="ab-flow-r">
+                    <div class="ab-flower-it">
+                        <h1>{{$info_page_translation->title}}</h1>
+                        <p>{!! $info_page_translation->content !!} </p>
+                    </div>
+                    <div class="ab-flower-it">
+                        <h1>Our vision</h1>
+                        <p>Pô Kô Farms determines to become a sustainable development cooperative, that helps farmers to have a better life through responsible coffee producing. Furthermore, Pô Kô Farms will be the first choice of customers who want to buy Fairtrade certified coffee in particular and agricutural  products in general from Vietnam.</p>
+                    </div>
+                    <div class="ab-flower-it">
+                        <h1>Our mission</h1>
+                        <p>Pô Kô Farms aims to become a cooperative that specializes in producing and supplying high quality Fairtrade certified coffee for current buyers and new customers in the future.
+By growing and selling Fairtrade certified coffee, Pô Kô Farms wishes to improve farmers income and quality of life for their  households .
+Sharing together and developing together, make the world better with quality coffee.
+ </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <img src="frontend/images/uploads/abflow-right.png" alt="">
+            </div>
+        </div>
+    </div>
+</div> -->
 
+<div class="about aboutv2 floweraboutv1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="heading-sec">
+                    <h1>{{$info_page_translation->title}}</h1>
+                    <h4 class="grey">-{!! $info_page_translation->description !!}-</h4>
+                </div>
+            </div>
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="ab-decorate">
+                    <img src="frontend/images/logo2.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="ab-intro">
+                    <p>{!! $info_page_translation->content !!} </p>
+                </div>  
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="ab-item">
+                    <img src="frontend/images/uploads/abflower1.png" alt="">
+                    <h2>Our vision</h2>
+                    <p>ô Kô Farms determines to become a sustainable development cooperative, that helps farmers to have a better life through responsible coffee producing. Furthermore, Pô Kô Farms will be the first choice of customers who want to buy Fairtrade certified coffee in particular and agricutural  products in general from Vietnam.</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="ab-item">
+                    <img src="frontend/images/uploads/abflower2.png" alt="">
+                    <h2>Our mission</h2>
+                    <p>Pô Kô Farms aims to become a cooperative that specializes in producing and supplying high quality Fairtrade certified coffee for current buyers and new customers in the future.
+By growing and selling Fairtrade certified coffee, Pô Kô Farms wishes to improve farmers income and quality of life for their  households .
+Sharing together and developing together, make the world better with quality coffee.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <!-- Info Page Section -->
-<section class="page-section" id="about">
+<!-- <section class="page-section" id="about">
     <div class="container relative">
         <div class="section-text mb-50 mb-sm-20">
             <div class="row">
@@ -62,7 +101,7 @@
             </div>
         </div>
     </div>
-</section>
+</section> -->
 
 <!-- End Info Page Section -->
 
